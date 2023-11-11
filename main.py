90cd2834929d22377ab25ce0d5b8318f63124ea7
kjanxloonix <kja@onet.eu>
2023-11-11 18:27:04 +0100

    Updated main and Processor class

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import argparse
-import pathlib
-
+# import argparse
+# import pathlib
 from processor import Processor
 
-# TODO argument parser
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="PCAP Processor")
-    parser.add_argument('-f', '--file_path', nargs='?', const='./samples/test.pcap', type=pathlib.Path)
-    args = parser.parse_args()
-    file_path = args.file_path.as_posix()
-else:
-    file_path = './samples/test.pcap'
 
-Processor(file_path).display_pcap()
+# TODO argument parser
+
+def main():
+    # parser = argparse.ArgumentParser(description="PCAP Processor")
+    # parser.add_argument('-f', '--file_path', nargs='?', const='./samples/test.pcap', type=pathlib.Path)
+    # args = parser.parse_args()
+    # file_path = args.file_path.as_posix()
+    file_path = r'./samples/test.pcap'
+    proc = Processor(file_path)
+    # proc.debug_display(6)
+    for i in proc.extract_conn_info():
+        print(i)
+    print(len(proc.extract_conn_info()))
+
+
+if __name__ == "__main__":
+    main()
--- processor.py
+++ processor.py
@@ -1,11 +1,41 @@
-from scapy.all import rdpcap
+from scapy.all import *
+from scapy.layers.inet6 import *
+from scapy.layers.l2 import ARP
+from collections import Counter
+
+
+def get_layers(p):
+    count = 0
+    while True:
+        lay = p.getlayer(count)
+        if lay is None:
+            break
+        yield lay
+        count += 1
 
 
 class Processor:
     def __init__(self, filepath):
-        self.filepath = filepath
+        self.path = filepath
+        self.cap = rdpcap(self.path)
 
-    def display_pcap(self):
-        cap = rdpcap(self.filepath)
-        for packet in cap:
-            print(packet)
+    def debug_display(self, i=int):
+        ls(self.cap[i])
+        # self.cap[i].show()
+
+    def extract_ip_addr(self, ip=IP):
+        list_of_ips = [p[ip].src for p in self.cap if ip in p]
+        list_of_ips.extend([p[ip].dst for p in self.cap if ip in p])
+        return dict(Counter(list_of_ips))
+
+    def extract_conn_info(self, ip=IP):
+        extract_list = []
+        for p in self.cap:
+            lays = [layer.name for layer in get_layers(p)]
+            if ARP in p:
+                extract_list.append({'layers': lays, 'ip_src': p[ARP].psrc, 'ip_dst': p[ARP].pdst})
+            if ip in p:
+                extract_list.append({'layers': lays, 'ip_src': p[ip].src, 'ip_dst': p[ip].dst})
+                if TCP in p or UDP in p:
+                    extract_list[-1].update({'s_port': p[ip].sport, 'd_port': p[ip].dport})
+        return extract_list
